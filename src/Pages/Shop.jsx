0f55ee5c37f6a74715375bfeb52050f094c6c02c
swastisunder <badatyaswastisunder@gmail.com>
2025-08-28 21:36:04 +0530

    Add Item, Offers, and Popular components with styling; update Shop page layout

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,9 +1,13 @@
 import Hero from "../Components/Hero/Hero";
+import Offers from "../Components/Offers/Offers";
+import Popular from "../Components/Popular/Popular";
 
 function Shop() {
   return (
     <div>
       <Hero />
+      <Popular />
+      <Offers />
     </div>
   );
 }
