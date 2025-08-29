298c4b165db8083082d356553e83fa5ca7a43337
swastisunder <badatyaswastisunder@gmail.com>
2025-08-29 13:22:00 +0530

    Add NewCollection and NewsLetter components with styling; update Shop page to include them

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,4 +1,6 @@
 import Hero from "../Components/Hero/Hero";
+import NewCollection from "../Components/NewCollection/NewCollection";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
 import Offers from "../Components/Offers/Offers";
 import Popular from "../Components/Popular/Popular";
 
@@ -8,6 +10,8 @@ function Shop() {
       <Hero />
       <Popular />
       <Offers />
+      <NewCollection />
+      <NewsLetter />
     </div>
   );
 }
