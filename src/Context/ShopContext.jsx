a0828f9c58001d57b322e8f80eef736fb056cfcb
swastisunder <badatyaswastisunder@gmail.com>
2025-09-02 20:52:45 +0530

    add to cart function

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,11 +1,34 @@
-import React, { createContext } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import all_products from "../Components/Assets/all_product";
 
 const ShopContext = createContext(null);
 
-const ShopContextProvider = (props) => {
-  const contextValue = { all_products };
+const getDefaultCart = () => {
+  let cart = {};
 
+  for (let index = 0; index < all_products.length + 1; index++) {
+    cart[index] = 0;
+  }
+
+  return cart;
+};
+
+const ShopContextProvider = (props) => {
+  const [cartItem, setCartItem] = useState(getDefaultCart());
+
+  const addToCart = (itemId) => {
+    setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  useEffect(() => {
+    console.log("Cart updated:", cartItem);
+  }, [cartItem]);
+
+  const contextValue = { all_products, cartItem, addToCart, removeFromCart };
   return (
     <ShopContext.Provider value={contextValue}>
       {props.children}
