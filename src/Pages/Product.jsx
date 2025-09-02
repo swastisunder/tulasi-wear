5d6cd26940cb8ba6d1a1efc4c43a61ea85e46d5a
swastisunder <badatyaswastisunder@gmail.com>
2025-09-02 20:10:36 +0530

    add DescriptionBox and RelatedProducts components, update Item and Product page

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -4,6 +4,8 @@ import { ShopContext } from "../Context/ShopContext";
 import { useParams } from "react-router-dom";
 import Breadcrums from "../Components/Breadcrums/Breadcrums";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
+import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 
 function Product() {
   const { all_products } = useContext(ShopContext);
@@ -14,6 +16,8 @@ function Product() {
     <div>
       <Breadcrums product={product} />
       <ProductDisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts />
     </div>
   );
 }
